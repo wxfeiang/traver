c93301a080dc03be199bb7af9abdced1123b524a
wxfeiang <wxfeiang@qq.com>
2019-06-05 21:39:43 +0800

    -6.5

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,18 +3,21 @@
         <home-header/>
         <HomeSwiper/>
         <home-icons/>
+        <Recommend/>
     </div>
 </template>
 <script>
 import HomeHeader from "./components/Header"
 import HomeSwiper from "./components/Swiper"
 import HomeIcons from "./components/Icons"
+import Recommend from "./components/Recommend"
 export default {
     name: "Home",
     components: {
         HomeHeader,
         HomeSwiper,
-        HomeIcons
+        HomeIcons,
+        Recommend
     }
 
 
